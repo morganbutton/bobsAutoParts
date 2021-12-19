e32d0cdac203e349a23e5229d468508f301111a5
DESKTOP-65PKKFE\morga <morgan button>
2021-12-19 15:04:33 -0500

    commit

---
+++ bobAutoForm/processorder.php
@@ -0,0 +1,25 @@
+<?php   
+    $tireqty =$_POST['tireqty'];
+    $oilqty =$_POST['oilqty'];
+    $sparkqty = $_POST['sparkqty'];
+    $totalqty = 0;
+    $totalamount = 0.00;
+
+    /* price doesnt change so we use constants*/
+    define('TIREPRICE', 100);
+    define('OILPRICE', 10);
+    define('SPARKPRICE', 4);
+    $totalqty = $tireqty + $oilqty + $sparkqty;
+    echo "<p> Your order is as follows: </p>";
+    echo htmlspecialchars($tireqty).' tires</br>';
+    echo htmlspecialchars($oilqty).' bottles of oil</br>';
+    echo htmlspecialchars($sparkqty).' spark plugs</br>';
+    echo "<p> Items Ordered: " .$totalqty."<br/>";
+    $totalamount = $tireqty * TIREPRICE
+                    + $oilqty * OILPRICE
+                    + $sparkqty * SPARKPRICE;
+    echo "Subtotal: $".number_format($totalamount, 2)."</p>";
+    echo "<p> Order Processed at";
+    echo date('H:i, jS F Y');
+    echo "</p>";
+?>
